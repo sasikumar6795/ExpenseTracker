450ec602690cb0c89b1a76ce68cecaa09c140b78
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2020-12-29 08:10:37 +0530

    created c,s,m

---
+++ expense-tracker-frontend/src/app/components/list-expenses/list-expenses.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-expenses',
+  templateUrl: './list-expenses.component.html',
+  styleUrls: ['./list-expenses.component.css']
+})
+export class ListExpensesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
